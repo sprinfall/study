2582f75b94185791465ae5eead069a70aa65c56c
sprinfall <sprinfall@gmail.com>
2016-03-30 17:32:29 +0800

    Add more samples

--- LearnObjC/LearnObjC/main.m
+++ LearnObjC/LearnObjC/main.m
@@ -11,44 +11,54 @@
 
 // NSArray
 
-void testNSArray() {
+void testNSArray_Create() {
 
-  // Create
-
+  // arrayWithObject
   {
     NSArray *someArray = [NSArray arrayWithObject:@"string0"];
     NSUInteger count = [someArray count];
     assert(count == 1);
   }
 
+  // arrayWithObjects
   {
     NSArray *someArray = [NSArray arrayWithObjects:@"string0", @"string1", @"string2", nil];
     NSUInteger count = [someArray count];
     assert(count == 3);
   }
 
+  // alloc ] init
   {
     NSArray *someArray = [[NSArray alloc] init];
     assert([someArray count] == 0);
   }
 
+  // new
+  {
+    NSArray *someArray = [NSArray new];
+    assert([someArray count] == 0);
+  }
+
+  // initWithObjects (end with nil)
   {
     NSArray *someArray = [[NSArray alloc] initWithObjects:@"string0", @"string1", @"string2", nil];
     NSUInteger count = [someArray count];
     assert(count == 3);
   }
 
+  // Literal syntax
   {
     NSArray *someArray = @[ @"string0", @"string1", @"string2" ];
     assert([someArray count] == 3);
   }
 
+  // Literal syntax - empty list
   {
     NSArray *someArray = @[];
     assert([someArray count] == 0);
   }
 
-
+  // initWithArray:copyItems:
   {
     NSMutableString *str0 = [NSMutableString stringWithString:@"string0"];
     NSMutableString *str1 = [NSMutableString stringWithString:@"string1"];
@@ -68,11 +78,93 @@ void testNSArray() {
 }
 
 
+void testNSArray_Query() {
+
+  {
+    NSArray *someArray = @[ @"string0", @"string1", @"string2" ];
+
+    assert([[someArray objectAtIndex:0] isEqualToString:@"string0"]);
+    assert([someArray[0] isEqualToString:@"string0"]);
+
+    BOOL contains = [someArray containsObject:@"string0"];
+    assert(contains == YES);
+
+    contains = [someArray containsObject:someArray[0]];
+    assert(contains == YES);
+
+    NSUInteger index = [someArray indexOfObject:@"string0"];
+    assert(index == 0);
+
+    index = [someArray indexOfObject:someArray[0]];
+    assert(index == 0);
+  }
+
+}
+
+
+void testNSArray_Sort() {
+
+  // Use sortedArrayUsingSelector
+  {
+    NSArray *numbers = @[ @9, @3, @2 ];  // Array of NSNumber
+
+    // NSNumber implements compare:
+    NSArray *sortedNumbers = [numbers sortedArrayUsingSelector:@selector(compare:)];
+    NSLog(@"Sorted numbers: %@", sortedNumbers);
+  }
+
+  // Use sortedArrayUsingComparator (block-based)
+  {
+    NSArray *numbers = @[ @9, @3, @2 ];
+
+    NSArray *sortedNumbers = [numbers sortedArrayUsingComparator:^(id obj1, id obj2) {
+      if ([obj1 integerValue] > [obj2 integerValue]) {
+        return NSOrderedDescending;
+      }
+      if ([obj1 integerValue] < [obj2 integerValue]) {
+        return NSOrderedAscending;
+      }
+      return NSOrderedSame;
+    }];
+
+    NSLog(@"Sorted numbers: %@", sortedNumbers);
+  }
+
+  // Use sortedArrayWithOptions (block-based)
+  {
+    NSArray *numbers = @[ @9, @3, @2 ];
+
+    // NSSortOptions: NSSortConcurrent | NSSortStable
+    NSArray *sortedNumbers = [numbers sortedArrayWithOptions:NSSortStable usingComparator:^(id obj1, id obj2) {
+      if ([obj1 integerValue] > [obj2 integerValue]) {
+        return NSOrderedDescending;
+      }
+      if ([obj1 integerValue] < [obj2 integerValue]) {
+        return NSOrderedAscending;
+      }
+      return NSOrderedSame;
+    }];
+
+    NSLog(@"Sorted numbers: %@", sortedNumbers);
+  }
+
+  // Use sortedArrayUsingSelector to sort strings case insensitively.
+  {
+      NSArray *strings = @[@"gammaString", @"alphaString", @"betaString"];
+      NSArray *sortedStrings = [strings sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)];
+      NSLog(@"Case insensitively sorted strings: %@", sortedStrings);
+  }
+
+}
+
+
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
     NSLog(@"Hello, World!");
 
-    testNSArray();
+    testNSArray_Create();
+    testNSArray_Query();
+    testNSArray_Sort();
   }
 
   return 0;
